52ef8d5035cc7e764ebf595e21e97d4ee5cb0c2f
mohamedhusman <tharikusman00@gmail.com>
2026-03-08 16:52:32 +0530

    feature add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello DevOps!");
+console.log("Iam learning CI/CD tests .");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Starting test ...");
+setTimeout(() => {
+  console.log("waiting 2 seconds...");
+}, 3000);
+console.log("starting tests ...s");
